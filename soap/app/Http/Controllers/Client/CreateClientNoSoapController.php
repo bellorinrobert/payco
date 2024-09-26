4a3a55f95df0e309acf52c0edcda0f4543dd7948
Robert Bellorin <rbellorin@integra.international>
2024-09-25 22:21:09 -0400

    Firma del controllador

--- soap/app/Http/Controllers/Client/CreateClientNoSoapController.php
+++ soap/app/Http/Controllers/Client/CreateClientNoSoapController.php
@@ -1,12 +1,15 @@
 <?php
-
+/**
+ * Summary of namespace App\Http\Controllers\Client
+ * @author Robert Bellorin <bellorinrobert@gmail.com>
+ * @date 2024-09-25 22:16:52
+ * 
+ */
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Client\CreateClientRequest;
 use App\Repositories\ClientRepository;
-
-
 
 class CreateClientNoSoapController extends Controller
 {
@@ -21,20 +24,19 @@ class CreateClientNoSoapController extends Controller
      */
     public function __invoke(CreateClientRequest $request)
     {
-        //
-        // try {
-            $data = $request->toArray();
+        
+        $data = $request->toArray();
 
-            $client = $this->clienteRepository->create($data);
-
-            return response()->json(data: [
+        $client = $this->clienteRepository->create($data);
+        
+        return response()->json(data: [
                 'meta' => [
                     'success' => true
                     , 'errors' => []
                 ], 'data' =>  $client
                 
-            ], status: 201);
-        // }
+            ]
+        , status: 201);        
         
     }
 }
