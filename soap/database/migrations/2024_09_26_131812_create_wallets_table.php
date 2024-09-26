cd179af2197f14d4ed686ba293c5c5658d0343f2
Robert Bellorin <rbellorin@integra.international>
2024-09-26 09:29:49 -0400

    Archivos faltantes

---
+++ soap/database/migrations/2024_09_26_131812_create_wallets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Client::class);
+            $table->decimal('balance',12,2)->default(0);
+            $table->enum('status', [
+                'a','b', 'c'
+            ])->default('a');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
---
+++ soap/database/migrations/2024_09_26_132307_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\Wallet;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Wallet::class);
+            $table->decimal('credit', 10, 2);
+            $table->decimal('debit', 10, 2);
+            $table->decimal('amount', 10,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
